48325f38c0efe64bbbd8d7d74aae4da83d2abf15
oibafs <fabioscaravelli@gmail.com>
2021-05-17 16:41:16 -0300

    Sync

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,7 @@ const options = {
   site: process.env.SITE || 'http://localhost:3000',
 
   // Configure one or more authentication providers
+/*
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_ID,
@@ -26,6 +27,13 @@ const options = {
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
     }),
+  ],
+*/
+  providers: [
+    Providers.Strava({
+      clientId: process.env.STRAVA_CLIENT_ID,
+      clientSecret: process.env.STRAVA_CLIENT_SECRET,
+    })
   ],
 
   // A database is optional, but required to persist accounts in a database
